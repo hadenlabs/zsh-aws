29e5f5e5b07de66defed3167d3b5aa21e13be8a8
Luis Mayta <luis@hadenlabs.com>
2020-09-19 21:06:40 -0500

    chore: config path files (#10)

---
+++ config/main.zsh
@@ -0,0 +1,19 @@
+#!/usr/bin/env ksh
+# -*- coding: utf-8 -*-
+
+function aws::config::main::factory {
+    # shellcheck source=/dev/null
+    source "${ZSH_AWS_SRC_PATH}"/config/base.zsh
+    case "${OSTYPE}" in
+    darwin*)
+        # shellcheck source=/dev/null
+        source "${ZSH_AWS_SRC_PATH}"/config/osx.zsh
+        ;;
+    linux*)
+        # shellcheck source=/dev/null
+        source "${ZSH_AWS_SRC_PATH}"/config/linux.zsh
+      ;;
+    esac
+}
+
+aws::config::main::factory
