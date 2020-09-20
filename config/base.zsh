1589acdc3013984e6dcea80fc000cfe01aeb5046
@slovacus <slovacus@gmail.com>
2020-09-19 21:06:40 -0500

    chore: config path files (#10)

---
+++ config/base.zsh
@@ -0,0 +1,5 @@
+#!/usr/bin/env ksh
+# -*- coding: utf-8 -*-
+
+export AWS_MESSAGE_BREW="Please install brew or use antibody bundle luismayta/zsh-brew branch:develop"
+export AWS_MESSAGE_NOT_FOUND="this not found installed"
