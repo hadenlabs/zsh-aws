835b575b5138359fdc382759c9bdee17d9cf3fe7
@slovacus <slovacus@gmail.com>
2021-06-18 23:36:28 -0500

    chore: implement aws go packages (#55)

--- config/base.zsh
+++ config/base.zsh
@@ -1,5 +1,10 @@
 #!/usr/bin/env ksh
 # -*- coding: utf-8 -*-
 
-export AWS_MESSAGE_BREW="Please install brew or use antibody bundle luismayta/zsh-brew branch:develop"
+export AWS_MESSAGE_GO_NOT_FOUND="Please install go or use antibody bundle hadenlabs/zsh-goenv branch:develop"
 export AWS_MESSAGE_NOT_FOUND="this not found installed"
+
+export AWS_GO_PACKAGES=(
+    github.com/99designs/aws-vault
+    github.com/minamijoyo/myaws/myaws
+)
