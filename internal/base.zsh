d2f48951d773ea34012b2f864346db108dea8e0c
@slovacus <slovacus@gmail.com>
2021-06-18 23:33:58 -0500

    chore: implement install dependences packages aws using go (#55)

---
+++ internal/base.zsh
@@ -0,0 +1,15 @@
+#!/usr/bin/env ksh
+# -*- coding: utf-8 -*-
+
+function aws::internal::packages::install {
+    if ! core::exists go; then
+        message_warning "${AWS_MESSAGE_GO_NOT_FOUND}"
+        return
+    fi
+
+    for package in "${AWS_GO_PACKAGES[@]}"; do
+       GO111MODULE=on go get -u -v "${package}"
+    done
+
+    message_success "Installed required Go packages"
+}
