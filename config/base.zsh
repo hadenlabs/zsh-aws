4fff91fff1403f13f559fb7039f554d553ad07ec
Luis Mayta <luis@hadenlabs.com>
2022-12-13 00:15:49 -0500

    chore: implement awless (#60)
    * refactor: lints code validation with pre-commit (#53)
    
    * chore: implement generator readme (#53)
    
    * refactor: docs rst to markdown (#53)
    
    * chore: implement generator changelog (#53)
    
    * refactor: travis to github action (#53)
    
    * refactor: configuration docker and docker-compose (#53)
    
    * refactor: actions make (#53)
    
    * refactor: configuration tmuxinator and pipfile (#53)
    
    * build: clean delete files test unnecessary (#53)
    
    * build: generate files (#53)
    
    * fix: version actions pre commit (#53)
    
    * Delete .github/workflows/lint.yml
    
    * Delete .github/workflows/release.yml
    
    * build: implement lint code github actions
    
    * build: implement release code github actions
    
    * refactor: lints code validation (#55)
    
    * refactor: generator template (#55)
    
    * refactor: actions git for make (#55)
    
    * refactor: name path zsh aws (#55)
    
    * chore: implement install dependences packages aws using go (#55)
    
    * chore: implement aws go packages (#55)
    
    * fix: links missing functions and pull requests (#55)
    
    * build: generate files project (#55)
    
    * chore: implement install awless (#60)
    
    * refactor: change get to install using go (#60)
    
    * feat: clean files configuration (#60)
    
    Co-authored-by: @dum-u <dum-u@hadenlabs.com>

--- config/base.zsh
+++ config/base.zsh
@@ -1,10 +1,11 @@
 #!/usr/bin/env ksh
 # -*- coding: utf-8 -*-
 
-export AWS_MESSAGE_GO_NOT_FOUND="Please install go or use antibody bundle hadenlabs/zsh-goenv branch:develop"
+export AWS_MESSAGE_GO_NOT_FOUND="Please install go or use antibody bundle hadenlabs/zsh-goenv"
 export AWS_MESSAGE_NOT_FOUND="this not found installed"
 
 export AWS_GO_PACKAGES=(
-    github.com/99designs/aws-vault
-    github.com/minamijoyo/myaws/myaws
+    github.com/99designs/aws-vault@latest
+    github.com/minamijoyo/myaws@latest
+    github.com/wallix/awless@latest
 )
